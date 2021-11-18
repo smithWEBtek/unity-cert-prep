27380cc90af6fd7644a9bf31c13c1b7b7e70555e
smithb1 <bradley.smith@hmhco.com>
2021-11-18 14:28:55 -0500

    Trigger area

---
+++ Ex_Files_Cert_Prep_Unity_Game_Dev/Exercise Files/Car/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Checkpoint : MonoBehaviour
+{
+
+    public Text UIText = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        UIText.text = "Checkpoint reached!";
+    }
+}
